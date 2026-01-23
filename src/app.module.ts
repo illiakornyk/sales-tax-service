75e0d8d430ed697f3145e9031c187358ba14f246
Illia Kornyk <krekerbrow@gmail.com>
2026-01-23 18:25:53 +0200

    feat: check for required envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,20 @@ import { GeographyModule } from './geography/geography.module';
 import { TaxRatesModule } from './tax-rates/tax-rates.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), GeographyModule, TaxRatesModule],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (config) => {
+        const required = ['ADMIN_API_KEY', 'CLIENT_API_KEY', 'DATABASE_URL'];
+        const missing = required.filter((key) => !config[key]);
+        if (missing.length) {
+          throw new Error(`Missing env vars: ${missing.join(', ')}`);
+        }
+        return config;
+      },
+    }),
+    GeographyModule,
+    TaxRatesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
