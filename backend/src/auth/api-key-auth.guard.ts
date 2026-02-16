2d9a08052e54c8881cc2b7b8864e84aa67eb7d02
Illia Kornyk <krekerbrow@gmail.com>
2026-02-16 12:59:14 +0200

    refactor: type for roles

--- backend/src/auth/api-key-auth.guard.ts
+++ backend/src/auth/api-key-auth.guard.ts
@@ -4,8 +4,9 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { ROLES, type UserRole } from './constants/roles.constants';
 
-type ApiUser = { role: 'ADMIN' };
+type ApiUser = { role: UserRole };
 
 @Injectable()
 export class ApiKeyAuthGuard implements CanActivate {
@@ -22,7 +23,7 @@ export class ApiKeyAuthGuard implements CanActivate {
     }
 
     if (apiKey === process.env.ADMIN_API_KEY) {
-      request.user = { role: 'ADMIN' };
+      request.user = { role: ROLES.ADMIN };
       return true;
     }
 
